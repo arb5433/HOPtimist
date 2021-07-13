717c0792d90c52f76d036739b27feed815decc60
adambailey <arb5433@gmail.com>
2021-07-13 16:58:32 -0400

    “.”

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,9 +3,19 @@ import SignupFormModal from "../SignupFormModal"
 
 function SplashPage(){
   return (
-    <div className='splash-page-banner'>
-      <LoginFormModal/>
-      <SignupFormModal/>
+    <div>
+      <div className='splash-page-banner'>
+        <LoginFormModal/>
+        <SignupFormModal/>
+      </div>
+      <div>
+        <form className='search-form' onSubmit={handleSubmit}>
+          <input className='search-input' value={search} onChange={(event) => setSearch(event.target.value)} placeholder='Search for a Beer'/>
+          <button className='search-icon' type='submit'>
+            <i className='fa fa-search'></i>
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
