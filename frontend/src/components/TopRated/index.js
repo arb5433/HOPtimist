d70e48ba980fd56d9689c223d10f960e9026774f
adambailey <arb5433@gmail.com>
2021-07-14 15:55:40 -0400

    “.”

---
+++ frontend/src/components/TopRated/index.js
@@ -0,0 +1,5 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from 'react';
+import { getBeers } from '../../store/beers';
+import BeerCard from '../BeerCard/index';
+
